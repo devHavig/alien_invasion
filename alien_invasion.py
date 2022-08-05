43716f5f14f30571cbaec47aec32837c6c99b2d6
JerrodxHavig <jhavig99@gmail.com>
2022-08-05 15:04:02 -0700

    debugging

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 
+
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
 
@@ -13,17 +14,17 @@ class AlienInvasion:
 
     def run_game(self):
         """Start the main loop for the game."""
-        while true:
+        while True:
             # Watch for Keyboard and Mouse events.
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
 
-        # Make the most recently drawn screen visible.
-        pygame.display.flip()
+            # Make the most recently drawn screen visible.
+            pygame.display.flip()
 
 
-if __name__ == '_main_':
+if __name__ == '__main__':
     # Make a game instance,and run the game.
     ai = AlienInvasion()
     ai.run_game()
